4b6b5efc960212a9b7272a45b57d50bed215568b
godwinvc <godwin@swift.local>
2016-09-19 18:06:53 +0530

    Added Touch functionality

--- src/directives.js
+++ src/directives.js
@@ -1,4 +1,41 @@
-angular.module('dragAndDropApp')
+angular.module('360iFrame')
+    .directive('multipleResponsive', function () {
+        return {
+            restrict: 'E',
+            link: function ($scope, elm, attr) {
+                var elem = elm[0];
+                [].forEach.call(elem.querySelectorAll('[type = "checkbox"]'), function (item) {
+                    item.onclick = function () {
+                        var arr = [];
+                        console.log(item);
+                        [].forEach.call(elem.querySelectorAll('[type = "checkbox"]'), function (check) {
+                            if (check.checked) {
+                                arr.push(check.value);
+                                elem.setAttribute('user-answer', arr.toString());
+                            }
+                        })
+
+                    }
+                })
+            }
+        }
+    })
+    .directive('multipleChoice', function () {
+        return {
+            restrict: 'E',
+            link: function ($scope, elm, attr) {
+
+                var item = elm[0];
+
+                [].forEach.call(item.querySelectorAll('[type = "radio"]'), function (elm) {
+                    elm.onclick = function () {
+                        item.setAttribute('user-answer', elm.value);
+                        var userAns = item.getAttribute('user-answer');
+                    }
+                });
+            }
+        }
+    })
     .directive('draganddropContainer', function () {
         return {
             restrict: 'E',
@@ -8,34 +45,23 @@ angular.module('dragAndDropApp')
                     [].forEach.call(_this.querySelectorAll('[drag-item]'), function (dragItem, dragItemIndex, dragItemArr) {
                         dragItem.style.position = 'absolute';
                         dragItem.draggable = false;
-                        dragItem.style.cursor = 'move';
+                        dragItem.style.cursor = 'pointer';
                         var coords = {
                             item: dragItem.getAttribute('drag-item'),
                             x: dragItem.offsetLeft,
                             y: dragItem.offsetTop
                         }
                         initial.push(coords);
-                        dragItem.onmousedown = function (e) {
-                            that = e.currentTarget;
-                            container = that.parentElement;
-                            var loop = setInterval(function () {
-                                if (container.nodeName !== 'DRAGANDDROP-CONTAINER') {
-                                    container = container.parentElement;
-                                } else {
-                                    clearInterval(loop);
-                                }
-                            }, 1);
-                            if (that.className.indexOf('transition') >= 0) {
-                                that.className = that.className.replace(' transition', '');
-                            }
-                            that.style.zIndex = 1000;
-                            pos_x = document.all ? window.event.clientX : e.pageX;
-                            pos_y = document.all ? window.event.clientY : e.pageY;
-                            that.style.left = pos_x - (that.offsetWidth / 2) + 'px';
-                            that.style.top = pos_y - (that.offsetHeight / 2) + 'px';
-                        }
-                        document.onmousemove = drag;
-                        document.onmouseup = dragend;
+
+                        //mouse events
+                        dragItem.addEventListener('mousedown', dragStart);
+                        document.addEventListener('mousemove', drag);
+                        document.addEventListener('mouseup', dragend);
+
+                        //touch events
+                        dragItem.addEventListener('touchstart', dragStart);
+                        document.addEventListener('touchmove', drag);
+                        document.addEventListener('touchend', dragend);
                     });
                     [].forEach.call(_this.querySelectorAll('[drop-zone]'), function (dropZone, dropZoneIndex, dropZoneArr) {
                         var coords = {
@@ -51,11 +77,34 @@ angular.module('dragAndDropApp')
                         dropZoneLocations.push(coords);
                     });
                 });
-
+                function dragStart(e) {
+                    that = e.currentTarget;
+                    container = that.parentElement;
+                    var loop = setInterval(function () {
+                        if (container.nodeName !== 'DRAGANDDROP-CONTAINER') {
+                            container = container.parentElement;
+                        } else {
+                            clearInterval(loop);
+                        }
+                    }, 1);
+                    if (that.className.indexOf('transition') >= 0) {
+                        that.className = that.className.replace(' transition', '');
+                    }
+                    that.style.zIndex = 1000;
+                    pos_x = document.all ? window.event.clientX : e.pageX;
+                    pos_y = document.all ? window.event.clientY : e.pageY;
+                    that.style.left = pos_x - (that.offsetWidth / 2) + 'px';
+                    that.style.top = pos_y - (that.offsetHeight / 2) + 'px';
+                }
                 function drag(e) {
                     if (that) {
-                        pos_x = document.all ? window.event.clientX : e.pageX;
-                        pos_y = document.all ? window.event.clientY : e.pageY;
+                        if (e.type == 'touchmove') {
+                            pos_x = e.changedTouches[0].pageX;
+                            pos_y = e.changedTouches[0].pageY;
+                        } else {
+                            pos_x = document.all ? window.event.clientX : e.pageX;
+                            pos_y = document.all ? window.event.clientY : e.pageY;
+                        }
                         that.style.left = pos_x - (that.offsetWidth / 2) + 'px';
                         that.style.top = pos_y - (that.offsetHeight / 2) + 'px';
                     }
